2cb6dd06ef82aff0693e24494dda0427c360bcaa
evanjaco <evanjaco@mtu.edu>
2024-12-09 15:13:47 -0500

    Prevent duplication of common functions

--- common.php
+++ common.php
@@ -1,16 +1,18 @@
 <?php
-function sanitizeInput($data) {
-    return htmlspecialchars(strip_tags(trim($data)));
-}
+if(!function_exists("sanitizeInput")) {
+    function sanitizeInput($data) {
+        return htmlspecialchars(strip_tags(trim($data)));
+    }
 
-function redirect($url) {
-    header("Location: $url");
-    exit;
-}
+    function redirect($url) {
+        header("Location: $url");
+        exit;
+    }
 
-function checkLogin() {
-    if (!isset($_SESSION['user_id'])) {
-        redirect('../login.php');
+    function checkLogin() {
+        if (!isset($_SESSION['user_id'])) {
+            redirect('../login.php');
+        }
     }
 }
 ?>
