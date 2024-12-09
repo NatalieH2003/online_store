97e044e7802b753bc1d66b63c92506537b8dea3b
evanjaco <evanjaco@mtu.edu>
2024-12-09 15:39:12 -0500

    Add reset_password link to customer main

--- customer/main.php
+++ customer/main.php
@@ -6,10 +6,11 @@ include '../common.php';
 
 checkLogin();
 
-echo "<h1>Welcome to the Customer Dashboard</h1>";
-echo "<a href='browse.php'>Browse Products</a><br>";
-echo "<a href='cart.php'>View Shopping Cart</a><br>";
-echo "<a href='orders.php'>View Orders</a><br>";
-echo "<a href='../logout.php'>Logout</a>";
 ?>
 
+<h1>Welcome to the Customer Dashboard</h1><br>
+<a href='browse.php'>Browse Products</a><br>
+<a href='cart.php'>View Shopping Cart</a><br>
+<a href='orders.php'>View Orders</a><br>
+<a href='../logout.php'>Logout</a><br>
+<a href='reset_password.php'> Reset Password</a><br>
