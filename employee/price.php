734456b083188f91876e53c4fac552b4d6d4c2a3
evanjaco <evanjaco@mtu.edu>
2024-12-09 21:24:27 -0500

    Track Price History

--- employee/price.php
+++ employee/price.php
@@ -7,9 +7,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $productId = sanitizeInput($_POST['product_id']);
     $newPrice = sanitizeInput($_POST['price']);
 
+    $stmt = $pdo->prepare("select price from products where id = ?");
+    $stmt->execute([$productId]);
+    $old_price = $stmt->fetch();
+
     // Update price
     $stmt = $pdo->prepare("UPDATE products SET price = ? WHERE id = ?");
     $stmt->execute([$newPrice, $productId]);
+
+    $stmt = $pdo->prepare("insert into price_history (product_id, old_price, new_price, change_date) values (?, ?, ?, CURRENT_TIMESTAMP())");
+    $stmt->execute([$productId, $old_price, $newPrice]);
 
     echo "Price updated successfully.";
 }
